f6c857e22ca6a375b7ba5cea42d5aaf2f9a1d010
Shogo Yasuda <shgysd@gmail.com>
2019-08-20 23:43:52 +0900

    Chapter-1

---
+++ Chapter-1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListeAndServe:", err)
+	}
+}
